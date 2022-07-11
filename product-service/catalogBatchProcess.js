78f7d8353ad19b75790a5dae4ea45c63f6bfb8b3
Gena888 <theGena888@gmail.com>
2022-07-12 01:37:39 +0300

    feat: add tests

--- product-service/catalogBatchProcess.js
+++ product-service/catalogBatchProcess.js
@@ -1,8 +1,27 @@
 import AWS from "aws-sdk";
 
+export const makeNotification = async (message) => {
+  const sns = new AWS.SNS({ region: 'eu-west-1' })
+  return sns.publish({
+    Subject: 'Product has been published',
+    Message: message,
+    TopicArn: process.env.SNS_ARN,
+    MessageAttributes: {
+      image: {
+        DataType: 'String',
+        StringValue: message?.image ? "true" : "false",
+      }
+    }
+  }).promise()
+    .then(() => {
+      console.log('Send email for: ', JSON.stringify(JSON.parse(item.body)));
+    }).catch((error) => {
+      console.log('We\'ve got some error: ', error)
+    })
+}
+
 export const catalogBatchProcess = (event, context, callback) => {
   const lambda = new AWS.Lambda({ region: 'eu-west-1' });
-  const sns = new AWS.SNS({ region: 'eu-west-1' })
 
   event.Records.forEach((item) => {
     lambda.invoke({
@@ -11,26 +30,10 @@ export const catalogBatchProcess = (event, context, callback) => {
     }, async (error, data) => {
       if (error) { context.done('lambda.invoke error', error) }
       if (data.Payload) {
-        await sns.publish({
-          Subject: 'Product has been published',
-          Message: JSON.stringify(item.body),
-          TopicArn: process.env.SNS_ARN,
-          MessageAttributes: {
-            image: {
-              DataType: 'String',
-              StringValue: JSON.parse(item.body).image ? "true" : "false",
-            }
-          }
-        }).promise()
-          .then(() => {
-            console.log('Send email for: ', JSON.stringify(JSON.parse(item.body)));
-          }).catch((error) => {
-            console.log('We\'ve got some error: ', error)
-          })
+        await makeNotification(JSON.stringify(item.body))
         context.succeed(data.Payload)
       }
     });
-
     console.log('product posted');
   })
 }
